33b500282b9b8c7abd06586d9530e2e77f92fcb8
Arancha Ferrero <aranchunfli11foz@hotmail.com>
2017-11-23 16:46:59 +0100

    Creada clase ColumnaService

---
+++ app/services/ColumnaService.java
@@ -0,0 +1,11 @@
+package services;
+
+
+import models.ColumnaRepository;
+import models.TableroRepository;
+
+public class ColumnaService {
+  ColumnaRepository columnaRepository;
+  TableroRepository tableroRepository;
+
+}
---
+++ test/sg8-columnas-en-tableros/ServicioColumnaTest.java
@@ -0,0 +1,28 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+import play.db.jpa.*;
+
+import play.inject.guice.GuiceApplicationBuilder;
+import play.inject.Injector;
+import play.inject.guice.GuiceInjectorBuilder;
+import play.Environment;
+
+import services.ColumnaService;
+
+public class ServicioColumnaTest {
+  static private Injector injector;
+
+  @BeforeClass
+  static public void initApplication() {
+     GuiceApplicationBuilder guiceApplicationBuilder =
+         new GuiceApplicationBuilder().in(Environment.simple());
+     injector = guiceApplicationBuilder.injector();
+     // Necesario para inicializar JPA
+     injector.instanceOf(JPAApi.class);
+  }
+
+  private ColumnaService newColumnaService() {
+    return injector.instanceOf(ColumnaService.class);
+  }
+}
