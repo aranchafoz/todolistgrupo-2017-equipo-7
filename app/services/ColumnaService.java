deb69fcbd371cc31aa293ddc1ec5a32c9db93c84
Arancha Ferrero <aranchunfli11foz@hotmail.com>
2017-11-23 17:08:25 +0100

    Añadido método obtener todas las columnas de un tablero

--- app/services/ColumnaService.java
+++ app/services/ColumnaService.java
@@ -1,5 +1,13 @@
 package services;
 
+import javax.inject.*;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+
+import models.Columna;
+import models.Tablero;
 
 import models.ColumnaRepository;
 import models.TableroRepository;
@@ -8,4 +16,20 @@ public class ColumnaService {
   ColumnaRepository columnaRepository;
   TableroRepository tableroRepository;
 
+  @Inject
+  public ColumnaService(ColumnaRepository columnaRepository, TableroRepository tableroRepository) {
+    this.columnaRepository = columnaRepository;
+    this.tableroRepository = tableroRepository;
+  }
+
+  public List<Columna> allColumnasTablero(Long idTablero) {
+    Tablero tablero = tableroRepository.findById(idTablero);
+    if (tablero == null) {
+      throw new ColumnaServiceException("tablero no existente");
+    }
+    List<Columna> columnas = new ArrayList<Columna>();
+    columnas.addAll(tablero.getColumnas());
+    Collections.sort(columnas, (a, b) -> a.getId() < b.getId() ? -1 : a.getId() == b.getId() ? 0 : 1);
+    return columnas;
+  }
 }
--- test/sg8-columnas-en-tableros/ServicioColumnaTest.java
+++ test/sg8-columnas-en-tableros/ServicioColumnaTest.java
@@ -3,6 +3,16 @@ import static org.junit.Assert.*;
 
 import play.db.jpa.*;
 
+import org.dbunit.*;
+import org.dbunit.dataset.*;
+import org.dbunit.dataset.xml.*;
+import org.dbunit.operation.*;
+import java.io.FileInputStream;
+
+import java.util.List;
+
+import models.Columna;
+
 import play.inject.guice.GuiceApplicationBuilder;
 import play.inject.Injector;
 import play.inject.guice.GuiceInjectorBuilder;
@@ -22,7 +32,25 @@ public class ServicioColumnaTest {
      injector.instanceOf(JPAApi.class);
   }
 
+  @Before
+  public void initData() throws Exception {
+     JndiDatabaseTester databaseTester = new JndiDatabaseTester("DBTest");
+     IDataSet initialDataSet = new FlatXmlDataSetBuilder().build(new FileInputStream("test/resources/usuarios_dataset.xml"));
+     databaseTester.setDataSet(initialDataSet);
+     databaseTester.setSetUpOperation(DatabaseOperation.CLEAN_INSERT);
+     databaseTester.onSetup();
+  }
+
   private ColumnaService newColumnaService() {
     return injector.instanceOf(ColumnaService.class);
+  }
+
+  // Test 1: allColumnasTableroEstanOrdenadas
+  @Test
+  public void allColumnasTableroEstanOrdenadas() {
+     ColumnaService columnaService = newColumnaService();
+     List<Columna> columnas = columnaService.allColumnasTablero(1001L);
+     assertEquals("Columna test 1", columnas.get(0).getNombre());
+     assertEquals("Columna test 2", columnas.get(1).getNombre());
   }
 }
